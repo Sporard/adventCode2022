93778ab120210dfd6c89d59f6972ec85649ca9ac
Sporard <pierresabard@gmail.com>
2022-12-04 21:04:46 +0100

    day 1 finish

--- day_1/day1.py
+++ day_1/day1.py
@@ -2,7 +2,7 @@
 
 
 
-def main():
+def part_1():
     print('Starting the probleme')
     print('opening file')
     f = open('input.txt', 'r')
@@ -11,7 +11,6 @@ def main():
     for line in f:
         print(line)
         if line.strip():
-            print(line)
             somme = somme + int(line)
         else:
             array.append(somme)
@@ -19,5 +18,19 @@ def main():
 
     print('max is : ')
     print(max(array))
+    return array
+def part_2(elves_list):
+    print('begining of part 2 \n')
+    maximum = []
+    for i in range(0,3):
+        maximum.append(max(elves_list))
+        elves_list.pop(elves_list.index(max(elves_list)))
+    print(maximum)
+    print('sum of the calories')
+    print(sum(maximum))
+
+def main():
+    elves_list = part_1()
+    part_2(elves_list)
 
 main()
